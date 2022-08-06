a9f1b3dc5efe798d155cedaccefc436679bfb3da
Irfandi Rustam <irfandy4hmad@gmail.com>
2022-08-06 12:41:10 +0800

    login test POM

---
+++ PageObjects/Locators.py
@@ -0,0 +1,7 @@
+# Login Page Elements
+txt_loginUserName = "name:Email"
+txt_loginPassword = "name:Password"
+btn_Login = "xpath://button[normalize-space()='Log in']"
+
+# johndoe12345@nopCommerce.com
+# customer12345
