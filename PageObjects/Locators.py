0881f4a8fd069d44796ad2d8c572b2bf18d4b687
Irfandi Rustam <irfandy4hmad@gmail.com>
2022-08-06 13:48:45 +0800

    registration test POM

--- PageObjects/Locators.py
+++ PageObjects/Locators.py
@@ -5,3 +5,18 @@ btn_Login = "xpath://button[normalize-space()='Log in']"
 
 # johndoe12345@nopCommerce.com
 # customer12345
+
+# Registration Page Elements
+link_register = "link:Register"
+rdBtn_male = "id:gender-male"
+rdBtn_female = "id:gender-female"
+txt_firstName = "name:FirstName"
+txt_lastName = "name:LastName"
+txt_email = "name:Email"
+drp_dobDay = "name:DateOfBirthDay"
+drp_dobMonth = "name:DateOfBirthMonth"
+drp_dobYear = "name:DateOfBirthYear"
+txt_company = "name:Company"
+txt_password = "name:Password"
+txt_confirmPassword = "name:ConfirmPassword"
+btn_register = "id:register-button"
